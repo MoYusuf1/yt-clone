900a63a0bf1b8dd2684495346186ca45b1127234
Mohamed Yusuf <101303178+MoYusuf1@users.noreply.github.com>
2024-07-28 07:57:45 +0300

    made skeleton of navbar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <>
-      Youtube Clone
+      MyTube
     </>
   );
 }
--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import { Contrast, Menu} from "lucide-react";
+import Link from "next/link";
+
 const TopNavigation = () => {
   return (
     <nav className="fixed top-0 left-0 w-screen z-20 dark:bg-black bg-white">
         <div className="flex justify-between items-center px-2 md:px-7 h-16">
+            <div className="flex items-center">
+                <span className="hover:bg-background-dark/30 md:block hidden hover:text-white cursor-pointer rounded-full p-2 mr-1">
+                <Menu size={30}/>
+                </span>
+
+                <Link href="/" className="flex items-center space-x-2">
+                <Contrast size={48} className="text-green"/>
+                <span className="hidden md:block text-2xl font-bold">MyTube</span>
+                </Link>
+
+            </div>
 
         </div>
     </nav>
