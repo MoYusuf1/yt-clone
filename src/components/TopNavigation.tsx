886f451d408d4df580dd3547b9cef68b3e876c0e
Mohamed Yusuf <101303178+MoYusuf1@users.noreply.github.com>
2024-07-29 18:58:32 +0300

    worked even more on the nav bar

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -1,9 +1,20 @@
 "use client";
 
-import { Contrast, Menu} from "lucide-react";
+import { Contrast, Menu, Search} from "lucide-react";
 import Link from "next/link";
+import { FormEvent, useRef } from "react";
 
 const TopNavigation = () => {
+    const searchInputRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+    }
+
+    if(searchInputRef.current) {
+        console.log(searchInputRef.current.value)
+    }
+
   return (
     <nav className="fixed top-0 left-0 w-screen z-20 dark:bg-black bg-white">
         <div className="flex justify-between items-center px-2 md:px-7 h-16">
@@ -18,8 +29,16 @@ const TopNavigation = () => {
             </div>
 
             <div className="md:flex items-center justify-center hidden">
-                <form onSubmit={} className="flex items-center h-10 mx-auto">
-                    
+                <form onSubmit={handleSubmit} className="flex items-center h-10 mx-auto">
+                    <input 
+                    type="search" 
+                    placeholder="Search" 
+                    ref={searchInputRef} 
+                    className="px-4 h-full md:w-48 dark:bg-gray-50 border-gray-300 rounded-l-md text-black focus:outline-none"
+                    />
+                    <div className="h-full px-5 grid place-content-center bg-background-LIGHT rounded-r-md text-black">
+                        <Search/>
+                    </div>
                 </form>
             </div> 
         </div>
