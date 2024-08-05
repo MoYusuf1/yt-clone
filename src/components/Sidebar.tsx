a4adfde1e0efc20f36a3449319e2f90ad2113bca
Mohamed Yusuf <101303178+MoYusuf1@users.noreply.github.com>
2024-08-05 09:54:00 +0200

    FINALLY FIXED THE HIDDEN MENU ICON. The codebase for lari is wrong, maybe he did it on purpose so the project is not fully copyable :p

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,19 @@
-import { ScrollArea } from '@/components/ui/ScrollArea';
-import { cn } from '@/lib/utils';
+import { ScrollArea } from "@/components/ui/ScrollArea";
+import { cn } from "@/lib/utils";
 import {
   Clock4,
   Compass,
   Contrast,
   History,
   Home,
+  Menu,
   MonitorPlay,
   Tv2,
   UserCircle,
-} from 'lucide-react';
-import Link from 'next/link';
-import { Button } from '@/components/ui/Button';
-import { Separator } from '@/components/ui/Separator';
+} from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/Button";
+import { Separator } from "@/components/ui/Separator";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -20,79 +21,84 @@ const Sidebar = ({ className }: Props) => {
   return (
     <ScrollArea
       className={cn(
-        'h-screen w-60 pt-3 z-40 !fixed hidden md:block top-0 dark:bg-black dark:text-white bg-white text-black -translate-x-full transition-transform duration-500',
+        "h-screen w-60 pt-3 z-40 !fixed hidden md:block top-0 dark:bg-black dark:text-white bg-white text-black -translate-x-full transition-transform duration-500",
         className
       )}
     >
-      <div className='px-5 flex items-center'>
-        <Link href='/' className='flex items-center space-x-2'>
-          <Contrast size={48} className='text-white' />
-          <span className='text-2xl font-black'>MYTube</span>
+      <div className="px-5 flex items-center">
+        <span className="hover:bg-background-dark/30 hover:text-white cursor-pointer rounded-full p-2 mr-1">
+          <Menu size={30} className="text-black dark:text-white" />
+        </span>
+        <Link href="/" className="flex items-center space-x-2">
+          <Contrast size={48} className="text-black dark:text-white" />
+          <span className="hidden md:block text-2xl font-bold text-black dark:text-white">
+            MYtube
+          </span>
         </Link>
       </div>
-      <div className='space-y-4 py-4'>
-        <div className='px-3 py-2'>
-          <h2 className='mb-2 px-4 text-lg font-semibold tracking-tight'>
+      <div className="space-y-4 py-4">
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
             Discover
           </h2>
-          <div className='space-y-1'>
-            <Link href='/'>
+          <div className="space-y-1">
+            <Link href="/">
               <Button
-                variant='default'
-                className='w-full font-light justify-start dark:bg-background-dark dark:text-white bg-background-light hover:bg-background-light/50 text-black'
+                variant="default"
+                className="w-full font-light justify-start dark:bg-background-dark dark:text-white bg-background-light hover:bg-background-light/50 text-black"
               >
-                <Home size={30} className='mr-3' />
+                <Home size={30} className="mr-3" />
                 Home
               </Button>
             </Link>
-            <Button variant='ghost' className='w-full justify-start font-light'>
-              <Compass size={30} className='mr-3' />
+            <Button variant="ghost" className="w-full justify-start font-light">
+              <Compass size={30} className="mr-3" />
               Explore
             </Button>
-            <Button variant='ghost' className='w-full justify-start font-light'>
-              <Tv2 size={30} className='mr-3' />
+            <Button variant="ghost" className="w-full justify-start font-light">
+              <Tv2 size={30} className="mr-3" />
               Subscriptions
             </Button>
           </div>
         </div>
         <Separator />
-        <div className='px-3 py-2'>
-          <h2 className='mb-2 px-4 text-lg font-semibold tracking-tight'>
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
             You
           </h2>
 
-          <div className='space-y-1'>
+          <div className="space-y-1">
             <Link href={`/channels/${process.env.NEXT_PUBLIC_CHANNEL_ID}`}>
               <Button
-                variant='ghost'
-                className='w-full justify-start font-light'
+                variant="ghost"
+                className="w-full justify-start font-light"
               >
-                <UserCircle size={30} className='mr-3' />
+                <UserCircle size={30} className="mr-3" />
                 Your channel
               </Button>
             </Link>
-            <Button variant='ghost' className='w-full justify-start font-light'>
-              <History size={30} className='mr-3' />
+            <Button variant="ghost" className="w-full justify-start font-light">
+              <History size={30} className="mr-3" />
               History
             </Button>
-            <Button variant='ghost' className='w-full justify-start font-light'>
-              <MonitorPlay size={30} className='mr-3' />
+            <Button variant="ghost" className="w-full justify-start font-light">
+              <MonitorPlay size={30} className="mr-3" />
               Your videos
             </Button>
-            <Button variant='ghost' className='w-full justify-start font-light'>
-              <Clock4 size={30} className='mr-3' />
+            <Button variant="ghost" className="w-full justify-start font-light">
+              <Clock4 size={30} className="mr-3" />
               Watch later
             </Button>
           </div>
         </div>
         <Separator />
-        <div className='px-3 py-2'>
-          <h2 className='mb-2 px-4 text-lg font-semibold tracking-tight'>
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
             SUBSCRIPTIONS
           </h2>
-          <div className='space-y-1'>
-            <Button variant='ghost' className='w-full justify-start font-light'>
-              <UserCircle size={30} className='mr-3' />
+          <div className="space-y-1">
+            <Button variant="ghost" className="w-full justify-start font-light">
+              <UserCircle size={30} className="mr-3" />
               Mohamed Yusuf
             </Button>
           </div>
--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -31,12 +31,12 @@ const TopNavigation = () => {
     <nav className="fixed top-0 left-0 w-screen z-20 dark:bg-black bg-white">
       <div className="flex justify-between items-center px-2 md:px-7 h-16">
         <div className="flex items-center">
-          <span className="hover:bg-background-dark/30 md:block hidden hover:text-white cursor-pointer rounded-full p-2 mr-1">
-            <Menu size={30} />
+          <span className="hover:bg-background-dark/30 hover:text-white cursor-pointer rounded-full p-2 mr-1">
+            <Menu size={30} className="text-black dark:text-white"/>
           </span>
           <Link href="/" className="flex items-center space-x-2">
-            <Contrast size={48} className="text-green" />
-            <span className="hidden md:block text-2xl font-bold">MYtube</span>
+            <Contrast size={48} className="text-black dark:text-white" />
+            <span className="hidden md:block text-2xl font-bold text-black dark:text-white">MYtube</span>
           </Link>
         </div>
 
@@ -57,7 +57,7 @@ const TopNavigation = () => {
           </form>
         </div>
 
-        <div className="flex items-center space-x-7">
+        <div className="flex items-center space-x-7 dark:text-white text-black">
           <div className="md:hidden">
             <ThemeToggle />
           </div>
