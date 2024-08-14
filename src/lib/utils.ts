270e109ba4ceafb41fec916a2be527075a2a4bf5
Mohamed Yusuf <mymohamedyusuf@gmail.com>
2024-08-14 15:40:06 -0500

    forgot to add the floor part :p

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,26 +19,25 @@ export function formatPublishedDate(publishedDate: string): string {
   const currentDate = new Date();
   const published = new Date(publishedDate);
 
-  const secondsAgo = Math.floor(
-    currentDate.getTime() - published.getTime() / 1000
-  );
+  const secondsAgo =
+    Math.floor(currentDate.getTime() - published.getTime()) / 1000;
 
   if (secondsAgo < 60) {
-    return `${secondsAgo} second${secondsAgo === 1 ? "" : "s ago"}`;
+    return `${secondsAgo} second${secondsAgo === 1 ? '' : 's'} ago`;
   } else if (secondsAgo < 3600) {
     const minutesAgo = Math.floor(secondsAgo / 60);
-    return `${minutesAgo} minute${minutesAgo === 1 ? "" : "s ago"}`;
+    return `${minutesAgo} minutes${minutesAgo === 1 ? '' : 's'} ago`;
   } else if (secondsAgo < 86400) {
     const hoursAgo = Math.floor(secondsAgo / 3600);
-    return `${hoursAgo} hour${hoursAgo === 1 ? "" : "s ago"}`;
+    return `${hoursAgo} hour${hoursAgo === 1 ? '' : 's'} ago`;
   } else if (secondsAgo < 2592000) {
     const daysAgo = Math.floor(secondsAgo / 86400);
-    return `${daysAgo} day${daysAgo === 1 ? "" : "s ago"}`;
-  } else if (secondsAgo < 315360000) {
+    return `${daysAgo} day${daysAgo === 1 ? '' : 's'} ago`;
+  } else if (secondsAgo < 31536000) {
     const monthsAgo = Math.floor(secondsAgo / 2592000);
-    return `${monthsAgo} month${monthsAgo === 1 ? "" : "s ago"}`;
+    return `${monthsAgo} months${monthsAgo === 1 ? '' : 's'} ago`;
   } else {
     const yearsAgo = Math.floor(secondsAgo / 315360000);
-    return `${yearsAgo} year${yearsAgo === 1 ? "" : "s ago"}`;
+    return `${yearsAgo} years${yearsAgo === 1 ? '' : 's'} ago`;
   }
 }
