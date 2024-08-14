daa68d03fa797afa5f41aada8d622dbe3e4f4170
Mohamed Yusuf <mymohamedyusuf@gmail.com>
2024-08-14 15:55:27 -0500

    i got scared that my code broke when I added the error handling but all it was just my query limit with google's api lol

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -14,17 +14,20 @@ export default function Home() {
     isLoading,
   } = useSWR("fetchVideo/${badge}", () => fetchVideos(badge, 9));
 
-
-  console.log(videoResults);
+  if(error){
+    throw new Error("Error fetching video data")
+  }
 
   return (
     <>
       <div className="px-2 md:pl-[252px] fixed top-16 py-2 left-0 w-screen dark:bg-black bg-white">
+        Video Results
       </div>
 
-      <div className="flex flex-wrap"> 
-        {videoResults?.map(video => <Thumbnail key={video.id} video={video}/>)}
-      
+      <div className="flex flex-wrap">
+        {videoResults?.map((video) => (
+          <Thumbnail key={video.id} video={video} />
+        ))}
       </div>
     </>
   );
